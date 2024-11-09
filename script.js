be4f22c9ad9800f97d56adc5f9b4b42e19748417
Jago971 <matt.mannings97@gmail.com>
2024-11-09 22:20:55 +0000

    working - functions, selects correct squares for airport. needs guarentee of squares for airport

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class Square {
-    constructor(generation, parent) {
-        squareStats(this, generation);
+    constructor(generation, parent, airport = false) {
+        squareStats(this, generation, airport);
 
         const newSquare = createSquare(this, parent);
 
@@ -8,16 +8,42 @@ class Square {
     }
 }
 
-function squareStats(square, generation) {
+function squareStats(square, generation, airport) {
     square.generation = generation;
+    square.airport = airport;
     square.probabilityPass =
         square.generation * 0.25 - 1 < Math.random() ? true : false;
-    square.divisions =
-        square.generation !== 1 ? Math.floor(Math.random() * 3) + 2 : 4;
-    square.layout =
-        square.divisions !== 4 ? (Math.random() < 0.5 ? "row" : "col") : null;
-    square.direction =
-        square.divisions !== 4 ? (Math.random() < 0.5 ? "for" : "rev") : null;
+
+    if (generation === 1) {
+        square.divisions = 4;
+        square.layout = null;
+        square.direction = null;
+    } else if (generation < 7) {
+        if (airport) {
+            let divisions = Math.random() < 0.5 ? 3 : 4;
+            square.divisions = divisions;
+            if (divisions === 3) {
+                let layout = Math.random() < 0.5 ? "row" : "col";
+                square.layout = layout;
+                square.direction = "rev";
+            } else {
+                square.layout = null;
+                square.direction = null;
+            }
+        } else {
+            let divisions = Math.floor(Math.random() * 3) + 2;
+            square.divisions = divisions;
+            square.layout =
+                divisions !== 4 ? (Math.random() < 0.5 ? "row" : "col") : null;
+            square.direction =
+                divisions !== 4 ? (Math.random() < 0.5 ? "for" : "rev") : null;
+        }
+    } else {
+        square.divisions = 1;
+        square.layout = null;
+        square.direction = null;
+    }
+
     const colors = [
         "rgb(0,75,0)",
         "rgb(0,100,0)",
@@ -31,12 +57,41 @@ function fillContents(square, newSquare) {
     let contents = [];
     for (let index = 0; index < square.divisions; index++) {
         if (square.generation < 7) {
-            if (square.probabilityPass) {
-                contents.push(new Square(square.generation + 1, newSquare));
+            if (square.airport) {
+                if (square.generation === 1) {
+                    if (index === 0) {
+                        contents.push(
+                            new Square(square.generation + 1, newSquare, true)
+                        );
+                    } else {
+                        contents.push(
+                            new Square(square.generation + 1, newSquare)
+                        );
+                    }
+                } else {
+                    if (index === 3) {
+                        contents.push(
+                            new Square(square.generation + 1, newSquare, true)
+                        );
+                    } else {
+                        contents.push(
+                            new Square(square.generation + 1, newSquare)
+                        );
+                    }
+                }
+            } else {
+                if (square.probabilityPass) {
+                    contents.push(new Square(square.generation + 1, newSquare));
+                } else {
+                    newSquare.style.backgroundColor = square.color;
+                }
             }
         } else {
             newSquare.style.backgroundColor = square.color;
         }
+    }
+    if (square.airport) {
+        newSquare.style.border = "solid 1px yellow";
     }
     square.contents = contents;
 }
@@ -86,10 +141,11 @@ function createSquare(square, parent) {
     if (!square.probabilityPass) {
         newSquare.style.backgroundColor = square.color;
     }
+
     parent.appendChild(newSquare);
     return newSquare;
 }
 
 const start = document.querySelector(".wrap");
-const firstSquare = new Square(1, start);
+const firstSquare = new Square(1, start, true);
 console.log(firstSquare);
