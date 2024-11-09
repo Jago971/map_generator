d71483cf0861a39f7d85743a4441604f6596029f
Jago971 <matt.mannings97@gmail.com>
2024-11-09 21:57:56 +0000

    working

--- script.js
+++ script.js
@@ -1,122 +1,76 @@
 class Square {
-    constructor(generation, parent, airport = false) {
-        squareStats(this, generation, airport);
+    constructor(generation, parent) {
+        squareStats(this, generation);
 
-        const colors = [
-            "rgb(0,75,0)",
-            "rgb(0,100,0)",
-            "rgb(0,125,0)",
-            "rgb(95, 70, 55)",
-        ];
-        this.color = colors[Math.floor(Math.random() * 4)];
+        const newSquare = createSquare(this, parent);
 
-        const newSquare = createSquare(
-            parent,
-            this.generation,
-            this.divisions,
-            this.layout,
-            this.direction
-        );
-
-        fillContents(
-            this,
-            newSquare,
-            this.generation,
-            this.airport,
-            this.probabilityPass,
-            this.divisions,
-            this.color
-        );
+        fillContents(this, newSquare);
     }
 }
 
-function fillContents(
-    square,
-    newSquare,
-    generation,
-    probabilityPass,
-    airport,
-    divisions,
-    color
-) {
-    let contents = [];
-    for (let index = 0; index < divisions; index++) {
-        if (generation < 7) {
-            contents.push(new Square(generation + 1, newSquare));
-            square.contents = contents;
-        } else {
-            newSquare.style.backgroundColor = color;
-        }
-    }
-}
-
-function squareStats(square, generation, aiport) {
+function squareStats(square, generation) {
     square.generation = generation;
-    square.aiport = aiport;
     square.probabilityPass =
-        generation * 0.25 - 1 < Math.random() ? true : false;
+        square.generation * 0.25 - 1 < Math.random() ? true : false;
+    square.divisions =
+        square.generation !== 1 ? Math.floor(Math.random() * 3) + 2 : 4;
+    square.layout =
+        square.divisions !== 4 ? (Math.random() < 0.5 ? "row" : "col") : null;
+    square.direction =
+        square.divisions !== 4 ? (Math.random() < 0.5 ? "for" : "rev") : null;
+    const colors = [
+        "rgb(0,75,0)",
+        "rgb(0,100,0)",
+        "rgb(0,125,0)",
+        "rgb(95, 70, 55)",
+    ];
+    square.color = colors[Math.floor(Math.random() * 4)];
+}
 
-    if (generation === 1) {
-        square.divisions = 4;
-        square.layout = null;
-        square.direction = null;
-    } else if (generation < 7) {
-        if (aiport) {
-            let divisions = Math.random() < 0.5 ? 3 : 4;
-            square.divisions = divisions;
-            if (divisions === 3) {
-                let layout = Math.random() < 0.5 ? "row" : "col";
-                square.layout = layout;
-                square.direction = "rev";
-            } else {
-                square.layout = null;
-                square.direction = null;
+function fillContents(square, newSquare) {
+    let contents = [];
+    for (let index = 0; index < square.divisions; index++) {
+        if (square.generation < 7) {
+            if (square.probabilityPass) {
+                contents.push(new Square(square.generation + 1, newSquare));
             }
         } else {
-            let divisions = Math.floor(Math.random() * 3) + 2;
-            square.divisions = divisions;
-            square.layout =
-                divisions !== 4 ? (Math.random() < 0.5 ? "row" : "col") : null;
-            square.direction =
-                divisions !== 4 ? (Math.random() < 0.5 ? "for" : "rev") : null;
+            newSquare.style.backgroundColor = square.color;
         }
-    } else {
-        square.divisions = 1;
-        square.layout = null;
-        square.direction = null;
     }
+    square.contents = contents;
 }
 
-function createSquare(parent, generation, divisions, layout, direction) {
+function createSquare(square, parent) {
     let newSquare = document.createElement("div");
 
-    if (generation === 1) {
+    if (square.generation === 1) {
         newSquare.classList.add("square", "base");
     } else {
         newSquare.classList.add("square");
     }
 
-    switch (divisions) {
+    switch (square.divisions) {
         case 1:
             newSquare.style.display = "initial";
             break;
         case 2:
             newSquare.style.display = "flex";
-            layout === "row"
-                ? direction === "for"
+            square.layout === "row"
+                ? square.direction === "for"
                     ? (newSquare.style.flexDirection = "row")
                     : (newSquare.style.flexDirection = "row-reverse")
-                : direction === "for"
+                : square.direction === "for"
                 ? (newSquare.style.flexDirection = "column")
                 : (newSquare.style.flexDirection = "column-reverse");
             break;
         case 3:
             newSquare.style.display = "grid";
-            layout === "row"
-                ? direction === "for"
+            square.layout === "row"
+                ? square.direction === "for"
                     ? newSquare.classList.add("row", "for")
                     : newSquare.classList.add("row", "rev")
-                : direction === "for"
+                : square.direction === "for"
                 ? newSquare.classList.add("col", "for")
                 : newSquare.classList.add("col", "rev");
             break;
@@ -128,11 +82,14 @@ function createSquare(parent, generation, divisions, layout, direction) {
         default:
             console.warn("Invalid division count");
     }
+
+    if (!square.probabilityPass) {
+        newSquare.style.backgroundColor = square.color;
+    }
     parent.appendChild(newSquare);
     return newSquare;
 }
 
 const start = document.querySelector(".wrap");
-const airport = true;
-const firstSquare = new Square(1, start, airport);
+const firstSquare = new Square(1, start);
 console.log(firstSquare);
